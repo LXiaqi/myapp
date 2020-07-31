812e6fb3f032b84d26a9fca83b96ff7ad8087440
Y <18534799988@163.com>
2020-07-31 14:21:09 +0800

    状态管理

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import './demo/slive_demo.dart';
 import './demo/navigator_demo.dart';
 import './demo/form_demo.dart';
 import './demo/material_components.dart';
+import './demo/state/state_management_demo.dart';
 
 void main() => runApp(App());
 
@@ -19,12 +20,13 @@ class App extends StatelessWidget {
     return MaterialApp(
       // debugShowCheckedModeBanner: false,
       // home: NavigatorDemo(),
-      initialRoute: '/mdc',
+      initialRoute: '/state-management',
       routes: {
         '/': (context) => Home(),
         '/about': (context) => P1age(title: 'Ablot'),
         '/form': (context) => FormDemo(),
         '/mdc': (context) => MaterialComponents(),
+        '/state-management': (context) => StateManagementDemo(),
       },
       theme: ThemeData(
         primarySwatch: Colors.pink,
